910de4dd1211a1dabeed2ccdca0d31843b999945
roops <roops.pesh@gmail.com>
2023-07-15 18:28:05 +0100

    add lil-gui

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -1,11 +1,19 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { GUI } from "lil-gui";
 
 export const Scene = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // settings for dat gui
+    const settings = {
+      zoom: 1,
+      fustrumSize: 20,
+    };
+    const gui = new GUI();
+
     const sizes = {
       width: window.innerWidth,
       height: window.innerWidth / 1.5,
@@ -13,21 +21,24 @@ export const Scene = () => {
 
     if (!containerRef.current) return;
 
+    const frustumSize = 20;
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      sizes.width / sizes.height,
+    const aspect = window.innerWidth / (window.innerWidth / 1.5);
+    const camera = new THREE.OrthographicCamera(
+      (frustumSize * aspect) / -2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
       0.1,
-      1000
+      100
     );
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(sizes.width, sizes.height);
 
+    // resize viewport on resize
     const resizeViewport = () => {
       const width = window.innerWidth;
       const height = window.innerWidth / 1.5;
-
-      camera.aspect = width / height;
       camera.updateProjectionMatrix();
 
       renderer.setSize(width, height);
@@ -35,13 +46,33 @@ export const Scene = () => {
 
     window.addEventListener("resize", resizeViewport);
 
+    // gui for zoom
+    gui.add(settings, "zoom", 0, 3, 0.1).onChange((value: number) => {
+      camera.zoom = value;
+      camera.updateProjectionMatrix();
+    });
+
+    // gui for fustrum size
+    gui.add(settings, "fustrumSize", 0, 50, 1).onChange((value: number) => {
+      camera.left = (value * aspect) / -2;
+      camera.right = (value * aspect) / 2;
+      camera.top = value / 2;
+      camera.bottom = value / -2;
+      camera.updateProjectionMatrix();
+    });
+
     containerRef?.current.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
+    camera.position.z = 1;
+
+    const setZoom = (zoom: number) => {
+      camera.zoom = 1.5;
+      camera.updateProjectionMatrix();
+    };
 
     const animate = function () {
       requestAnimationFrame(animate);
